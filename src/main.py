ddba84069b1b720f484036f5beb232cea12a9626
Nivi <nivedini27@gmail.com>
2022-10-30 18:13:02 -0700

    add potry

--- src/main.py
+++ src/main.py
@@ -1,23 +1,15 @@
-import os
-import platform
-from helpers.common import print_name
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
 if __name__ == '__main__':
-    print(f"Ajay likes Nivi {1+1} times")
-    print(os.getcwd())
-    print(platform.system())
-    info = [{
-                "Name": "Nivi",
-                "Type": "jaffa",
-            },
-            {
-                "Name": "Aadhya",
-                "Type": "Good",
-            },
-            {
-                "Name": "Ajay",
-                "Type": "Good",
-            }]
-    for daffa in info:
-        print_name(name=daffa["Name"], typ=daffa["Type"])
+    driver.get("https://www.redfin.com/WA/Seattle/9255-Greenwood-Ave-N-98103/unit-38/home/26357")
+    time.sleep(3)
+    photos = driver.find_element(by=By.XPATH, value='//*[@id="photoPreviewButton"]/div')
+    photos.click()
+    time.sleep(10)
+    driver.quit()
